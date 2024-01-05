faa0b49db178cbe48283f59542f94e0900ac3cff
Marcin Nowak <malcinn@gmail.com>
2024-01-05 17:19:18 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+using var host = Host
+  .CreateDefaultBuilder(args)
+  .ConfigureLogging((_, logging) => logging.AddSerilog())
+  .UseSerilog((_, configuration) => configuration
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContextName}] {Message:lj}{NewLine}{Exception}")
+    .Enrich.FromLogContext()
+    .Enrich.WithComputed("SourceContextName", "Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)") // commenting out this line fixes build
+  )
+  .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Log with SourceContextName");
+
+await host.RunAsync();
